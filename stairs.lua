643eec07dff1490c54777f55d64650571ed11858
Oscar Ramos <oscar_rm7@hotmail.com>
2024-12-12 22:13:18 -0800

    Added iteration through map list

--- stairs.lua
+++ stairs.lua
@@ -3,7 +3,7 @@ require("map")
 
 Stairs = {}
 
-function Stairs:load()
+function Stairs:load(mapList)
     --load stairs sprite
     self.stairSprite = love.graphics.newImage("sprites/locked.png")
     self.locked = true
@@ -18,6 +18,10 @@ function Stairs:load()
     self.collider:setType("static")
     self.collider:setFixedRotation(true)
 
+    --map cycling
+    self.maplist = mapList or {}
+    self.currentMapIndex = 1
+
     -- make img clean
     love.graphics.setDefaultFilter("nearest", "nearest")
 end
@@ -27,27 +31,41 @@ function Stairs:update(dt)
     --(TODO change when all enemies dead)
     if self.collider:enter("Player") then
         if self.locked == false then
-            self:changeLevel("maps/floor1_2.lua")
+            self:advanceToNextMap()
+        else
+            self.stairSprite = love.graphics.newImage("sprites/open.png")
+            self.locked = false
         end
-        self.stairSprite = love.graphics.newImage("sprites/open.png")
-        self.locked = false
+    end
+end
+
+--update map index
+function Stairs:advanceToNextMap()
+    self.currentMapIndex = self.currentMapIndex + 1
+
+    --check if game has been won
+    if self.currentMapIndex > 10 then
+        print("Congratulations you won!!")
+        return
     end
 
-    --TODO if player touches stairs when open and presses enter change map
+    --pass new map to change level function
+    local nextMapFile = self.maplist[self.currentMapIndex]
+    self:changeLevel(nextMapFile)
+end
 
-    -- Switch map and reload associated objects
-    function Stairs:changeLevel(nextMap)
-        -- Clean up the current map objects
-        Map:unload()
-    
-        -- Load the new map
-        Map:load(nextMap)
-    
-        -- Reset stairs for the new map (if reused in multiple levels)
-        --Needs work
-        self.locked = true
-        self.stairSprite = love.graphics.newImage("sprites/locked.png")
-    end
+-- Switch map and reload associated objects
+function Stairs:changeLevel(nextMap)
+    -- Clean up the current map objects
+    Map:unload()
+
+    -- Load the new map
+    Map:load(nextMap)
+
+    -- Reset stairs for the new map (if reused in multiple levels)
+    --Needs work
+    self.locked = true
+    self.stairSprite = love.graphics.newImage("sprites/locked.png")
 end
 
 function Stairs:draw()
