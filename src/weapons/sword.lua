66ff746f3d4aa451a3e03021b76b1e5b4c586562
Jose Abrego <joseabrego13@gmail.com>
2024-12-21 02:07:06 -0600

    Rotate the sword depending on the player's direction

--- src/weapons/sword.lua
+++ src/weapons/sword.lua
@@ -21,6 +21,9 @@ function Sword:load(player)
 
 	-- Status
 	self.isActive = true -- Sword is only active during an attack
+
+	-- Rotation
+	self.rotation = 0
 end
 
 function Sword:update(dt)
@@ -42,25 +45,58 @@ end
 function Sword:move()
 	local offsetX, offsetY = 0, 0
 
-	-- Update sword position based on player's position and facing direction
-	if self.player.anim == self.player.animations.up then
-		offsetY = -12
-	elseif self.player.anim == self.player.animations.down then
-		offsetY = 12
-	elseif self.player.anim == self.player.animations.left then
-		offsetX = -12
-	elseif self.player.anim == self.player.animations.right then
-		offsetX = 12
+	-- Update sword position and rotation based on player's direction
+	if self.player.curr_direction == "up" then
+		offsetY = -24 -- Offset behind the player
+		offsetX = -3 -- Center
+		self.rotation = -math.pi / 2 -- Rotate 90 degrees counterclockwise
+		self.collider:destroy()
+		self.collider = world:newRectangleCollider(self.player.x + offsetX, self.player.y + offsetY, 6, 16)
+	elseif self.player.curr_direction == "down" then
+		offsetY = 8 -- Offset in front of the player
+		offsetX = -3 -- Center
+		self.rotation = math.pi / 2 -- Rotate 90 degrees clockwise
+		self.collider:destroy()
+		self.collider = world:newRectangleCollider(self.player.x + offsetX, self.player.y + offsetY, 6, 16)
+	elseif self.player.curr_direction == "left" then
+		offsetX = -24 -- Offset to the left
+		offsetY = -2 -- Center
+		self.rotation = math.pi
+		self.collider:destroy()
+		self.collider = world:newRectangleCollider(self.player.x + offsetX, self.player.y + offsetY, 16, 6)
+	elseif self.player.curr_direction == "right" then
+		offsetX = 8 -- Offset to the right
+		offsetY = -2 -- Center
+		self.rotation = 0
+		self.collider:destroy()
+		self.collider = world:newRectangleCollider(self.player.x + offsetX, self.player.y + offsetY, 16, 6)
 	end
 
-	self.collider:setPosition(self.player.x + offsetX, self.player.y + offsetY)
+	-- Deactivate the sword collider when not attacking
+	if not self.isActive then
+		self.collider:setCollisionClass("None") -- Temporarily disable collision
+	else
+		self.collider:setCollisionClass("Player Weapon") -- Enable collision when attacking
+	end
+
+	self.collider:setType("static")
 end
 
 function Sword:draw()
 	local spriteOffsetX = self.sprite:getWidth() / 2
 	local spriteOffsetY = self.sprite:getHeight() / 2
+
 	-- Only draw sword if it is currently active
 	if self.isActive then
-		love.graphics.draw(self.sprite, self.x - spriteOffsetX, self.y - spriteOffsetY)
+		love.graphics.draw(
+			self.sprite,
+			self.x,
+			self.y,
+			self.rotation, -- Apply rotation
+			1,
+			1, -- Scale
+			spriteOffsetX,
+			spriteOffsetY -- Center sprite at the rotation point
+		)
 	end
 end
